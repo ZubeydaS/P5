a2495fb8dea54366de0c988ea37d3faa50c33fd5
Zubeyda <shutez@carleton.edu>
2025-02-23 00:47:38 -0600

    new coord

--- myGame.js
+++ myGame.js
@@ -64,20 +64,20 @@ function highlightInteractiveZones() {
   rect(401, 518, 314, 135); // Bed
   rect(-31, 272, 227, 226); // TV
   rect(263, 339, 97, 78); // Frame
-  rect(284, 668, 447, 290); // Carpet
-  rect(81, 706, 149, 185); // Scratch Post
+  rect(289, 678, 444, 280); // Carpet
+  rect(80, 716, 506, 271); // Scratch Post
   rect(794, 714, 145, 267); // Toys
   rect(781, 320, 212, 86); // Pillow
   rect(634, 316, 288, 221); // Cat
   rect(734, 549, 42, 134);  // Cat Tail
-}
+} 
 
 function mousePressed() {
   if (checkInteraction(401, 518, 314, 135, 'bed', "You sit on the bed. It's surprisingly comfy!")) return;
   if (checkInteraction(-31, 272, 227, 226, 'tv', "You find something to watch on TV.")) return;
   if (checkInteraction(263, 339, 97, 78, 'frame', "You admire the framed picture.")) return;
-  if (checkInteraction(284, 668, 447, 290, 'carpet', "The carpet... perfect to ruin.")) return;
-  if (checkInteraction(81, 706, 149, 185, 'scratch_post', "I'd rather scratch the bed.")) return;
+  if (checkInteraction(289, 678, 444, 280, 'carpet', "The carpet... perfect to ruin.")) return;
+  if (checkInteraction(80, 716, 506, 271, 'scratch_post', "I'd rather scratch the bed.")) return;
   if (checkInteraction(794, 714, 145, 267, 'toys', "My favorite toys neatly piled :).")) return;
   if (checkInteraction(781, 320, 212, 86, 'pillow', "The pillow looks fluffy.")) return;
   if (checkInteraction(634, 316, 288, 221, 'cat', "Hey!")) return;
