a50e37a816d819ac9d7cf9df2dc23e09de518272
Zubeyda <shutez@carleton.edu>
2025-02-23 00:04:40 -0600

    new coordinates

--- myGame.js
+++ myGame.js
@@ -61,25 +61,25 @@ function highlightInteractiveZones() {
   strokeWeight(2);
 
   // Example coordinates; adjust these using your displayCoordinates()
-  rect(436, 520, 271, 174); // Bed
-  rect(3.44, 328, 199.56, 420); // TV
+  rect(401, 518, 314, 135); // Bed
+  rect(-31, 272, 227, 226); // TV
   rect(263, 339, 97, 78); // Frame
-  rect(415, 635, 187, 335); // Carpet
+  rect(332, 681, 349, 324); // Carpet
   rect(21.44, 731.61, 227.56, 114.39); // Scratch Post
-  rect(822, 710, 60, 283); // Toys
-  rect(800, 310, 172, 128); // Pillow
+  rect(794, 714, 145, 267); // Toys
+  rect(781, 320, 212, 86); // Pillow
   rect(634, 316, 288, 221); // Cat
   rect(734, 549, 42, 134);  // Cat Tail
 }
 
 function mousePressed() {
-  if (checkInteraction(436, 520, 271, 174, 'bed', "You sit on the bed. It's surprisingly comfy!")) return;
-  if (checkInteraction(3.44, 328, 199.56, 420, 'tv', "You find something to watch on TV.")) return;
+  if (checkInteraction(401, 518, 314, 135, 'bed', "You sit on the bed. It's surprisingly comfy!")) return;
+  if (checkInteraction(-31, 272, 227, 226, 'tv', "You find something to watch on TV.")) return;
   if (checkInteraction(263, 339, 97, 78, 'frame', "You admire the framed picture.")) return;
-  if (checkInteraction(415, 635, 187, 335, 'carpet', "The carpet... perfect to ruin.")) return;
+  if (checkInteraction(332, 681, 349, 324, 'carpet', "The carpet... perfect to ruin.")) return;
   if (checkInteraction(21.44, 731.61, 227.56, 114.39, 'scratch_post', "I'd rather scratch the bed.")) return;
-  if (checkInteraction(822, 710, 60, 283, 'toys', "My favorite toys neatly piled :).")) return;
-  if (checkInteraction(800, 310, 172, 128, 'pillow', "The pillow looks fluffy.")) return;
+  if (checkInteraction(794, 714, 145, 267, 'toys', "My favorite toys neatly piled :).")) return;
+  if (checkInteraction(781, 320, 212, 86, 'pillow', "The pillow looks fluffy.")) return;
   if (checkInteraction(634, 316, 288, 221, 'cat', "Hey!")) return;
   if (checkInteraction(734, 549, 42, 134, 'cat_tail', "That tickes >:(")) return;
 }
